90b8bd9be1088f93f99befb7e1a589aedd8cdebe
Danny Libin <Danny Libin>
2017-07-23 16:29:08 -0500

    legend

--- js/iter1.js
+++ js/iter1.js
@@ -1,23 +1,29 @@
 'use strict';
 
-const w = 1200;
-const h = 600;
+const w = 1000;
+const h = 500;
+let fullData;
 
 window.onload = init();
 
 function init() {
-  d3.csv('../workingLoan.csv', data => pivot(data, 'ProsperScore', 'IncomeRange', 'BorrowerAPR'));
+  d3.csv('../workingLoan.csv', csvdata => {
+    fullData = csvdata;
+    pivot('ProsperScore', 'IncomeRange', 'BorrowerAPR');
+  });
 }
 
 /**
  * Create a pivot table representation of data and draw
  * 
- * @param {any} data An array of objects with keys being column name
  * @param {any} index The desired index to pivot on.
  * @param {any} column The desired column to pivot on.
  * @param {any} value The field to aggregate via mean (intersection of index and column)
  */
-function pivot(data, index, column, value) {
+function pivot(index, column, value) {
+  let incomeRanges = ['$1-24,999', '$25,000-49,999', '$50,000-74,999', '$75,000-99,999', '$100,000+']
+
+  let data = fullData;
   // Nest data by index and column
  let agg = d3.nest()
           .key(d => d[index])
@@ -31,8 +37,10 @@ function pivot(data, index, column, value) {
   agg.forEach(prosperAgg => {
     prosperAgg.values.forEach(incomeAgg => {
       let pivotPoint = {}
+      // Transform axes to integers for scaling
       pivotPoint[index] = +prosperAgg.key;
-      pivotPoint[column] = incomeAgg.key;
+      pivotPoint[column] = incomeRanges.indexOf(incomeAgg.key) + 1;
+
       pivotPoint['value'] = incomeAgg.values;
       pivotData.push(pivotPoint);
     });
@@ -42,9 +50,12 @@ function pivot(data, index, column, value) {
 }
 
 function draw(data) {
-  
-  let valRange = d3.extent(data, d => d['value']);
-  
+
+  let colorScale = d3.scale.linear()
+    .domain(d3.extent(data, d => d['value']))
+    .interpolate(d3.interpolateHcl)
+    .range([d3.rgb('#ffff66'), d3.rgb('#028166')]);
+
   let xScale = d3.scale.ordinal()
     .domain(d3.range(1, 6))
     .rangeRoundBands([0, w]);
@@ -58,7 +69,7 @@ function draw(data) {
   let yAxisTicks = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11'];
   let yAxis = d3.svg.axis().scale(yScale).orient('left').tickFormat(d => yAxisTicks[d - 1]);
 
-  let svg = d3.select('.d3anchor')
+  let svg = d3.select('#d3anchor')
     .append('svg')
     .attr('width', w + 20)
     .attr('height', h)
@@ -80,23 +91,45 @@ function draw(data) {
     .append('rect')
     .attr('x', d => xScale(d['IncomeRange']))
     .attr('y', d => yScale(d['ProsperScore']))
-    .attr('width',  w / (data.length / 12) )
+    .attr('width',  w / (data.length / 11) )
     .attr('height', yScale.rangeBand())
-    .attr('fill', d => getCellColor(d['value'], valRange) );
-}
+    .attr('fill', d => colorScale(d['value']) );
 
-/**
- * Get cell color based on value using hsl scale
- * 
- * @param {any} value 
- * @param {any} valRange 
- */
-function getCellColor(value, valRange) {
-  let minColor = 60; // Yellow
-  let maxColor = 120; // Green
-  let range = maxColor - minColor;
+  // Legend
+  let valRange = d3.extent(data, d => d['value']);
+  let legendSteps = 10;
+  let legendData =  d3.range(valRange[0], valRange[1],
+                            (valRange[1] - valRange[0]) / legendSteps);
+  let legendCellWidth = 30;
+  let legendCellHeight = 20;
+  d3.select('#legend')
+    .append('svg')
+    .attr('width', legendCellWidth)
+    .attr('height', legendData.length * legendCellHeight)
+    .style('margin-left', '15px')
+    .selectAll('rect')
+    .data(legendData)
+    .enter()
+    .append('rect')
+    .attr('y', (d, i) => i * legendCellHeight + legendCellHeight)
+    .attr('height', legendCellHeight)
+    .attr('width', legendCellWidth)
+    .attr('fill', d => colorScale(d));
 
-  let scale = (value - valRange[0]) / valRange[1];
-  let hue = minColor + (range * scale)
-  return `hsl(${hue}, 100%, 50%)`
+  let legendTextSteps = 5;
+  let legendText = d3.range(valRange[0], valRange[1],
+                            (valRange[1] - valRange[0]) / legendTextSteps);
+  d3.select('#legend')
+    .append('svg')
+    .attr('width', '200px')
+    .attr('height', legendData.length * legendCellHeight)
+    .style('margin-left', '10px')
+    .selectAll('text')
+    .data(legendText)
+    .enter()
+    .append('text')
+    .attr('y', (d, i) => i * legendCellHeight * (legendSteps / legendTextSteps) + legendCellHeight * 2 - 4)
+    .text(d => {
+      return Math.round(d*100) + '%'
+    })
 }
